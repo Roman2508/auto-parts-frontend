fea83a42e833d4a072df50aa5e0d014468186549
Roman2508 <2508romaa@gmail.com>
2025-11-11 21:39:07 +0200

    fix

--- src/utils/string/get-review-word-with-ending.ts
+++ src/utils/string/get-review-word-with-ending.ts
@@ -2,6 +2,7 @@ export const getReviewWordWithEnding = (reviewCount: number) => {
   switch (reviewCount) {
     case 1 || 21 || 31:
       return `${reviewCount} відгук`
+    // @ts-ignore
     case 2 || 3 || 4 || 22 || 23 || 24 || 34:
       return `${reviewCount} відгуки`
     default:
